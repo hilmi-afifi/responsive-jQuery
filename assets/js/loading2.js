c6b6ba470c656e6aca731d11c8c014eda3fb2aa5
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2018-05-21 11:08:34 +0700

    Add files via upload

---
+++ assets/js/loading2.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".my-progress-bar").circularProgress({
+        line_width: 6,
+        color: "#000",
+        starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+        percent: 0, // percent starts from
+        percentage: true,
+        text: "Ini Merupakan Loading Page"
+    }).circularProgress('animate', 100, 5000);
+});
+	var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-36251023-1']);
+	  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
